108baa5b9532f07b41cda2430712509f8b65bc76
marc <nguyenvannhv26@gmail.com>
2026-01-11 02:11:25 +0700

    5.Implement echo

--- app/main.py
+++ app/main.py
@@ -6,9 +6,18 @@ def main():
     while(True):
         sys.stdout.write("$ ")
         command = input()
-        if command == 'exit':
-            break
-        print(f"{command}: command not found")
+        word_list = command.split()
+        first_word = word_list[0]
+        word_list.pop(0)
+
+        match first_word:
+            case 'echo':
+                string_to_print = ' '.join(word_list)
+                print(string_to_print)
+            case 'exit':
+                break
+            case _:
+                print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
