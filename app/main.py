dbc463479240beb546ddd4f7393018298e8d482f
marc <nguyenvannhv26@gmail.com>
2026-01-10 23:24:04 +0700

    2.Handle invalid commands

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
+    command = input()
+    print(f"{command}: command not found")
     pass
 
 
