48e34c390a6a6e9526a7ab3f25e3dddff5da631b
marc <nguyenvannhv26@gmail.com>
2026-01-11 11:30:36 +0700

    6.Implement type

--- app/main.py
+++ app/main.py
@@ -10,15 +10,36 @@ def main():
         first_word = word_list[0]
         word_list.pop(0)
 
+        command_map = {
+            'echo': echo_handler,
+            'type': type_handler,
+            'exit': lambda x: x
+        }
+
         match first_word:
             case 'echo':
-                string_to_print = ' '.join(word_list)
-                print(string_to_print)
+                arguments = ' '.join(word_list)
+                print(arguments)
+            case 'type':
+                arguments = ' '.join(word_list)
+                if(arguments in command_map.keys()):
+                    print(f'{arguments} is a shell builtin')
+                else:
+                    print(f'{arguments}: not found')
             case 'exit':
                 break
             case _:
                 print(f"{command}: command not found")
 
+def echo_handler(word_list: list[str]):
+    print(' '.join(word_list))
+
+def type_handler(word_list:list[str], command_map: dict):
+    arguments = ' '.join(word_list)
+    if(arguments in command_map.keys):
+        print(f'{arguments} is a shell builtin')
+    else:
+        print(f'{arguments}: not found')
 
 if __name__ == "__main__":
     main()
