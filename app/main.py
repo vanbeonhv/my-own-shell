5a6489f1f96ba1c59956db7b78184b60ce580dd7
marc <nguyenvannhv26@gmail.com>
2026-01-15 00:18:40 +0700

    8.Run a program

--- app/main.py
+++ app/main.py
@@ -1,16 +1,21 @@
 import os
+import subprocess
 import sys
-from unittest import result
+class FileDetail:
+    def __init__(self, name: str, path: str, is_executable: bool):
+        self.name = name
+        self.path = path
+        self.is_executable = is_executable
 
-# PATH="/usr/bin:/usr/local/bin:"
 
+PATHS = os.environ['PATH'].split(os.pathsep)
+# PATHS = ['D:\\apps\\Microsoft VS Code\\bin']
 def main():
     while(True):
         sys.stdout.write("$ ")
-        command = input()
-        word_list = command.split()
+        input_command = input()
+        word_list = input_command.split()
         first_word = word_list[0]
-        word_list.pop(0)
 
         command_map = {
             'echo': echo_handler,
@@ -22,32 +27,61 @@ def main():
             result = command_map[first_word](word_list, command_map)
             if(result == 'EXIT'): 
                 break
+            if(result == 'CONTINUE'):
+                continue
+        execute_check_result = check_executable_files(word_list[0])
+        if(execute_check_result.is_executable):
+            # sub process nhận là dạng list string ['fullpath_extention', 'argument']
+            subprocess.run([execute_check_result.name] + word_list[1:])
         else:
-            print(f"{command}: command not found")
+            print(f"{input_command}: command not found")
+
+# ================================================================
                 
 def echo_handler(word_list: list[str], context):
+    word_list.pop(0)
     print(' '.join(word_list))
+    return 'CONTINUE'
 
 def type_handler(word_list:list[str], command_map: dict):
-    arguments = ' '.join(word_list)
+    # need handle case 'type' + blank comand
+    main_command = word_list[1]
     is_found = False
-    if arguments in command_map:
-        print(f'{arguments} is a shell builtin')
+    if main_command in command_map:
+        print(f'{main_command} is a shell builtin')
         is_found = True
     else: 
-        paths = os.environ['PATH'].split(os.pathsep)
-        for path in paths:
-            full_path = os.path.join(path, arguments)
-            result = check_executable_files(full_path)
-            if(result):
-                print(f"{arguments} is {full_path}")
-                is_found = True
-                break
-
+        result = check_executable_files(main_command)
+        # print('result', result)
+        if(result.is_executable):
+            print(f"{main_command} is {result.path}")
+            is_found = True
     if(is_found is False): 
-        print(f'{arguments}: not found')
+        print(f'{main_command}: not found')
+    return 'CONTINUE'
 
-def check_executable_files(full_path: str):
+def check_executable_files(main_command: str):
+    for path in PATHS:
+        full_path = os.path.join(path, main_command)
+
+        if sys.platform == "win32":
+            return check_executable_file_window(full_path)
+        else:
+            is_executable = is_file_executable(full_path)
+            if(is_executable):
+                return FileDetail(main_command, full_path, is_executable)
+    return FileDetail('', '', False)
+
+def check_executable_file_window(full_path):
+    window_extensions = ['.exe', '.cmd']
+    for extension in window_extensions:
+        absolute_path = full_path + extension
+        is_executable = is_file_executable(absolute_path)
+        if(is_executable):
+            # ở window, để execute file thì phải truyền full path + extentions. Linux thì chỉ cần name
+            return FileDetail(absolute_path, absolute_path, is_executable)
+
+def is_file_executable(full_path: str):
     if(os.path.isfile(full_path)):
         return os.access(full_path, os.X_OK)
     return False
